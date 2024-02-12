b09d4033ad61c23dade3c19396232be35abcffe5
fatura-brigitta <fatura.brigitta@students.jedlik.eu>
2024-02-12 13:05:56 +0100

    eleje

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const canvas = document.getElementById('tetris');
+const context = canvas.getContext('2d');
+context.scale(20, 20);
+let esesInterval = 250;
+let fut = false;
+
+function matrixCsinal(szelesseg, magassag){
+    const matrix = [];
+    while (magassag--){
+        matrix.push(new Array(szelesseg).fill(0));
+    }
+    return matrix;
+}
+
+
 const shapes = [
     [
         [0, 1, 0, 0],
